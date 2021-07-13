873e88cb0943535ca04c593b91e72a463180e493
Ashley <ashchat2008@gmail.com>
2021-07-13 17:03:20 -0400

    Blog post

--- quoterumi/includes/section-blogcontent.php
+++ quoterumi/includes/section-blogcontent.php
@@ -1,41 +1,41 @@
+
 <?php if( have_posts()): while( have_posts()): the_post();?>
 
+  <div class="blog-post">
+    <?php
+        $fname  = get_the_author_meta('first_name');
+        $lname  = get_the_author_meta('last_name');
+      ?>
 
-  <?php
-      $fname  = get_the_author_meta('first_name');
-      $lname  = get_the_author_meta('last_name');
-    ?>
+      <span class="date"><?php echo get_the_date()?></span>
+      <p>Posted by: <span class="author-name"><?php echo $fname;?> <?php echo $lname;?></span></p>
 
-    <span class="date"><?php echo get_the_date()?></span>
-    <p>Posted by: <span class="author-name"><?php echo $fname;?> <?php echo $lname;?></span></p>
+      <div class="blog-content"><?php the_content(); ?></div>
 
-    <?php the_content(); ?>
+      <?php
+      $tags = get_the_tags();
+      foreach((array)$tags as $tag):?>
+
+          <a href="<?php echo get_tag_link($tag->term_id);?>" class="badge bg-success">
+              <?php echo $tag->name; ?>
+          </a>
+
+      <?php endforeach ?>
+
+
 
     <?php
-    $tags = get_the_tags();
-    foreach((array)$tags as $tag):?>
+      $categories = get_the_category();
+      foreach($categories as $cat):?>
 
-        <a href="<?php echo get_tag_link($tag->term_id);?>" class="badge bg-success">
-            <?php echo $tag->name; ?>
-        </a>
+      <a href="<?php echo get_category_link($cat->term_id);?>" class="badge bg-success">
+      <?php echo $cat->name;?>
+      </a>
+    <?php endforeach; ?>
 
-    <?php endforeach ?>
-
-
-
-  <?php
-    $categories = get_the_category();
-    foreach($categories as $cat):?>
-
-    <a href="<?php echo get_category_link($cat->term_id);?>" class="badge bg-success">
-    <?php echo $cat->name;?>
-    </a>
-  <?php endforeach; ?>
-
+    </div>
 
   <?php comments_template();?>
-
-  <?php //comments_template();?>
 
 
 
--- quoterumi/single.php
+++ quoterumi/single.php
@@ -4,14 +4,16 @@
 
 
         <?php if(has_post_thumbnail()):?>
+          <div class="blog-card card">
+            <div class="blog-img">
+            <img src="<?php the_post_thumbnail_url('blog-large');?>" alt="<?php the_title();?>" class="img-fluid img-tumbnail">
+            </div>
+          <?php endif;?>
 
-          <img src="<?php the_post_thumbnail_url('blog-large');?>" alt="<?php the_title();?>" class="img-fluid img-tumbnail">
+            <h1 class="blog-title"><?php the_title();?></h1>
 
-        <?php endif;?>
-
-        <h1><?php the_title();?></h1>
-
-        <?php get_template_part('includes/section','blogcontent');?>
+            <?php get_template_part('includes/section','blogcontent');?>
+        </div>
 </div>
 </section>
 
